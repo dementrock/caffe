bfe9ecdd4d1e636ef601c4e7560dfd3a7befa58d
Rocky Duan <dementrock@gmail.com>
2016-03-25 21:19:51 -0700

    adapt for dqn

--- include/caffe/solver.hpp
+++ include/caffe/solver.hpp
@@ -67,6 +67,7 @@ class Solver {
     return test_nets_;
   }
   int iter() { return iter_; }
+  void set_iter(int new_iter) { iter_ = new_iter; }
 
   // Invoked at specific points during an iteration
   class Callback {
@@ -84,7 +85,6 @@ class Solver {
 
   void CheckSnapshotWritePermissions();
 
- protected:
   // Make and apply the update value for the current iteration.
   virtual void ApplyUpdate() = 0;
   // The Solver::Snapshot function implements the basic snapshotting utility
@@ -95,6 +95,7 @@ class Solver {
   string SnapshotFilename(const string extension);
   string SnapshotToBinaryProto();
   string SnapshotToHDF5();
+ protected:
   // The test routine
   void TestAll();
   void Test(const int test_net_id = 0);
